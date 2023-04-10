0187ef42f912805bfb7721e4ebc2409c3801020d
Sam Jones <samejones2018@gmail.com>
2023-04-09 18:34:29 -0700

    buttons are sicker lmao

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//Define variables for the game and grab the elements from the html
+
+//Create a variable to hold your score
+let score = 0;
+//Create a variable to hold your highest score
+let highScore = 0;
+//create a variable to be the snake grab its id from the html
+
+
+//create a variable to be the food
+
+//set up a function to have the snake move
+
+//set up a function to have the snake eat the food
+
+//set up a function to have the snake grow
+
+//set up a function to have the snake die
+
+//set up game over function
+
+//set up high score board function enter name and store in local storage
+
+//set up a new game function to reset the game
